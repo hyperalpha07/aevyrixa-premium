0b0a6116214c0d129fb424923af0600e7b944f6d
RS AlphA YT <mkvaipotuhin@gmail.com>
2026-09-13 11:44:42 +0800

    Polish Noromi about background and surfaces

--- configs/brand/noromi-backgrounds.ts
+++ configs/brand/noromi-backgrounds.ts
@@ -36,6 +36,7 @@ export const noromiBackgroundAssets = Object.freeze({
   customerFlow02: backgroundAsset("customer-flow-bg-02"),
   customerFlow03: backgroundAsset("customer-flow-bg-03"),
   accountFlow01: backgroundAsset("account-flow-bg-01"),
+  aboutPage01: backgroundAsset("about-page-bg-01"),
 });
 
 /** Proposed page-role mapping for later, separately scoped visual phases. */
@@ -47,6 +48,7 @@ export const noromiBackgroundUsage = Object.freeze({
   checkout: noromiBackgroundAssets.customerFlow02,
   trackOrder: noromiBackgroundAssets.customerFlow03,
   accountLogin: noromiBackgroundAssets.accountFlow01,
+  about: noromiBackgroundAssets.aboutPage01,
   contentSupport: noromiBackgroundAssets.luxePlumSilkAndPetals,
 });
 
